612cec1189b163a6a5ae75f3bf7bb10765797d98
LeeRyangHwa <iryanghwa@gmail.com>
2021-01-05 16:39:42 +0900

    intellj commit test

--- src/main/java/com/springpro/api/controller/testRestController.java
+++ src/main/java/com/springpro/api/controller/testRestController.java
@@ -3,7 +3,7 @@ package com.springpro.api.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@RestController // restController test
 public class testRestController {
     @GetMapping("/api/hello")
     public String hello(){
